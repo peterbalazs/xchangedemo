86158762fa79eae402f8881e2c6cf79d5533a071
Peter Balazs <peter.d.balazs@gmail.com>
2017-04-09 13:24:51 +0200

    add webapp and spring boot configuration

---
+++ src/main/java/com/pbalazs/fxdemo/Application.java
@@ -0,0 +1,16 @@
+package com.pbalazs.fxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Peter on 4/9/2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
